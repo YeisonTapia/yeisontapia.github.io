9da7205ac8812848e5cd222acf6fbfb3a3955ccd
yeisontapia <yeison.tapia@sundevs.com>
2020-09-09 09:58:26 -0500

    Add home page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+import '../../css/main.css'
+
+export default function Home() {
+  return <div className="container">
+    <div>404</div>
+  </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
 import React from "react"
+import '../../css/main.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className="container">
+    <div className="item">
+      <span role="img" aria-label="emojis">
+        🇨🇴🧑🏽‍💻
+      </span>
+    </div>
+    <div className="item">
+      Yeison Fabian Tapia Lozano
+    </div>
+    <div className="item">
+      Software Developer
+    </div>
+    <div className="item">
+      <a 
+        href="https://github.com/yeisonTapia/" 
+        className="item-a" 
+        target="_blank" 
+        rel="noreferrer">
+        GitHub 
+      </a> 
+      <a 
+        href="https://github.com/yeisonTapia/" 
+        className="item-a" 
+        target="_blank" 
+        rel="noreferrer">
+        Twitter
+      </a> 
+    </div>
+  </div>
 }
