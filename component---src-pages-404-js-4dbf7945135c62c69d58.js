4908e3b22264d3a68502cbea70323d8f0bbaa24a
yeisontapia <yeison.tapia@sundevs.com>
2020-11-13 15:08:21 -0500

    Updates

---
+++ component---src-pages-404-js-4dbf7945135c62c69d58.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{hjJ8:function(n,e,t){},w2l6:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return i}));var a=t("q1tI"),c=t.n(a);t("hjJ8");function i(){return c.a.createElement("div",{className:"container"},c.a.createElement("div",null,"404"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4dbf7945135c62c69d58.js.map
